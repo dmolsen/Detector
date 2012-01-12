5356173496eb8b9c464a8b1985f41830d6a03a1a
Dave Olsen <dmolsen@gmail.com>
2012-01-12 17:14:44 -0500

    tweaked some of the wording

--- web/templates/_about.inc.php
+++ web/templates/_about.inc.php
@@ -2,6 +2,6 @@
 <p>
 	<a href="https://github.com/dmolsen/Detector">Detector</a> is a simple, PHP- and JavaScript-based browser- and feature-detection library. Detector gives
 	server-side developers information on what types of devices may be requesting their content as well as the HTML5 &amp; CSS3 features a requesting browser may or may not support. With Detector a developer 
-	can serve the appropriate markup, stylesheets, and JavaScript to a requesting browser without being completely dependent on front-end-only 
-	script loaders. <a href="https://github.com/dmolsen/Detector">Check out the README</a> for more information.
+	can serve the appropriate markup, stylesheets, and JavaScript to a requesting browser without being completely dependent on a browser-detection library being up-to-date nor completely dependent on front-end-only script loaders.
+	<a href="https://github.com/dmolsen/Detector">Check out the README</a> for more information.
 </p>
