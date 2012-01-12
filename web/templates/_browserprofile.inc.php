e94302246c230a2c2eedca5c5a5ea88cd1a60b73
Dave Olsen <dmolsen@gmail.com>
2012-01-12 17:15:28 -0500

    added support for the archive feature and made the row titles bold

--- web/templates/_browserprofile.inc.php
+++ web/templates/_browserprofile.inc.php
@@ -2,7 +2,9 @@
 <p>
 	The following browser profile was created using the browser-detection of Detector. It, as well as the the following feature profile, were <strong>
 	<?php
-		if (Detector::$foundIn == 'cookie') {
+		if (Detector::$foundIn == "archive") {
+			print " pulled from a profile already in the system that you asked to view. Because it's an archived profile the browser-side tests were not run.";
+		} else if (Detector::$foundIn == 'cookie') {
 			print " created when you first hit this page because Detector didn't recognize your user-agent. You may have experienced a very brief redirect when loading the page initially. The profiles have now been saved for use with other visitors.";
 		} else if (Detector::$foundIn == 'file') {
 			print " created in the past when another user with the same user agent visited this demo. Detector simply pulled the already existing information for your visit.";
@@ -19,43 +21,43 @@
 	</thead>
 	<tbody>
 		<tr>
-			<td class="span3">User Agent:</td>
+			<th class="span3">User Agent:</th>
 			<td><?=$ua->ua?></td>
 		</tr>
 		<tr>
-			<td class="span3">Gen. <? if ($ua->isMobile) { ?>OS<? } else { ?> Grouping<? } ?>:</td>
+			<th class="span3">Gen. <? if ($ua->isMobile) { ?>OS<? } else { ?> Grouping<? } ?>:</th>
 			<td><?=$ua->deviceOSGeneral?></td>
 		</tr>
 		<? if ($ua->isMobile) { ?>
 			<tr>
-				<td>Specific OS:</td>
+				<th>Specific OS:</th>
 				<td><?=$ua->deviceOSSpecific?></td>
 			</tr>
 		<? } ?>
 		<? if ($ua->majorVersion != 0) { ?>
 			<tr>
-				<td>Major Version:</td>
+				<th>Major Version:</th>
 				<td><?=$ua->majorVersion?></td>
 			</tr>
 			<tr>
-				<td>Minor Version:</td>
+				<th>Minor Version:</th>
 				<td><?=$ua->minorVersion?></td>
 			</tr>
 		<? } ?>
 		<tr>
-			<td>Is Mobile?</td>
+			<th>Is Mobile?</th>
 			<td><?=convertTF($ua->isMobile)?></td>
 		</tr>
 		<tr>
-			<td>Is Tablet?</td>
+			<th>Is Tablet?</th>
 			<td><?=convertTF($ua->isTablet)?></td>
 		</tr>
 		<tr>
-			<td>Is Computer?</td>
+			<th>Is Computer?</th>
 			<td><?=convertTF($ua->isComputer)?></td>
 		</tr>
 		<tr>
-			<td>Is Spider?</td>
+			<th>Is Spider?</th>
 			<td><?=convertTF($ua->isSpider)?></td>
 		</tr>
 	</tbody>
