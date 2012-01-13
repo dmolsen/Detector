36988ec4fe6ca74e5721b3cb98da5cb5279cab96
Dave Olsen <dmolsen@gmail.com>
2012-01-13 14:46:52 -0500

    adding the "fork me" graphic

--- web/templates/index.default.inc.php
+++ web/templates/index.default.inc.php
@@ -18,4 +18,8 @@
 	<?php include("templates/_archive.inc.php"); ?>
 </div>
 
+<div id="forkme">
+	<a href="https://github.com/dmolsen/Detector"><img src="images/ForkMe_Wht.png" width="141" height="141" alt="Fork Me on GitHub" /></a>
+</div>
+
 <?php include("templates/_footer.inc.php"); ?>
