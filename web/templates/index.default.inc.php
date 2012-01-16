7f0dfe3941b92c77b12e786df470c0144df1f494
Dave Olsen <dmolsen@gmail.com>
2012-01-16 10:55:24 -0500

    grammar check

--- web/templates/index.default.inc.php
+++ web/templates/index.default.inc.php
@@ -3,7 +3,7 @@
 <div class="span10">
 	<p>
 		With the initial release of <a href="http://yiibu.com/">Yiibu's</a> <a href="https://github.com/yiibu/profile">Profile</a>, <a href="https://github.com/dmolsen/Detector">Detector</a> is already <abbr title="Yet Another Browser- and Feature-Detection Library">YABFDL</abbr>
-		<em>(Yet Another Browser- and Feature-Detection Library)</em>. The concepts behind Detector have been floating around in my head since I heard Yiibu's talk, 
+		<em>(Yet Another Browser- and Feature-Detection Library)</em>. The concepts behind Detector have been floating around in my head ever since I heard Yiibu's talk, 
 		<a href="http://www.slideshare.net/yiibu/adaptation-why-responsive-design-actually-begins-on-the-server">Adaptation</a>. I've finally put my ideas into <a href="https://github.com/dmolsen/Detector">code</a> and have created this demo.
 		To learn more about <a href="https://github.com/dmolsen/Detector">Detector</a> and how it works please check out the <a href="https://github.com/dmolsen/Detector">README on GitHub</a>. 
 	</p>
