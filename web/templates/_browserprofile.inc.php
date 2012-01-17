3429a188c55fac611614bec772fc6c74578b4f8d
Dave Olsen <dmolsen@gmail.com>
2012-01-16 20:57:09 -0500

    a custom message for any user who hits detector with a browser that doesn't support JS

--- web/templates/_browserprofile.inc.php
+++ web/templates/_browserprofile.inc.php
@@ -7,7 +7,9 @@
 		} else if (Detector::$foundIn == 'cookie') {
 			print " created when you first hit this page because Detector didn't recognize your user-agent. You may have experienced a very brief redirect when loading the page initially. The profiles have now been saved for use with other visitors.";
 		} else if (Detector::$foundIn == 'file') {
-			print " created in the past when another user with the same user agent visited this demo. Detector simply pulled the already existing information for your visit.";
+			print " created in the past when another user with the same user-agent visited this demo. Detector simply pulled the already existing information for your visit.";
+		} else if (Detector::$foundIn == 'nojs') {
+			print " created when you first hit this page because Detector didn't recognize your user-agent. Because your browser didn't support JavaScript your profiles are very limited. The profiles have now been saved for use with other visitors.";
 		} else {
 			print " pulled from session because you've visited this page before.";
 		}
