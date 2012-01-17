5de6e99f92f1460b5ea4cb2b15fa7fd7823dcb86
Dave Olsen <dmolsen@gmail.com>
2012-01-17 09:19:37 -0500

    the archive feature on the demo now pulls from the ua list

--- web/templates/_archive.inc.php
+++ web/templates/_archive.inc.php
@@ -3,16 +3,12 @@
 	The following user agent profiles are already in the system:
 </p>
 <ul>
-<?php
-	if ($handle = opendir(__DIR__ .'/../../lib/Detector/user-agents-core/')) {
-	    while (false !== ($entry = readdir($handle))) {
-	        if ($entry != "." && $entry != ".." && $entry != "README" && $entry != "ua.template.json" && $entry != ".gitignore") {
-	            $uaJSONCore = file_get_contents(__DIR__."/../../lib/Detector/user-agents-core/".$entry);
-				$uaJSONCore = json_decode($uaJSONCore);
-				print "<li> <a href=\"/?pid=".$uaJSONCore->uaHash."\">".trim(substr($uaJSONCore->ua, 0, 28))."...</a></li>";
-	        }
-	    }
-	    closedir($handle);
-	}
-?>
+	<?php
+		if ($uaListJSON = @file_get_contents(__DIR__."/../../lib/Detector/user-agents-core/ua.list.json")) {
+			$uaList = json_decode($uaListJSON);
+			foreach ($uaList as $key => $value) {
+				print "<li> <a href=\"/?pid=".$key."\">".trim(substr($value, 0, 28))."...</a></li>";
+			}
+		}
+	?>
 </ul>
