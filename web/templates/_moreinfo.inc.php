fbff064e2cbb495fb6418920d425bd80a9e8545b
Dave Olsen <dmolsen@gmail.com>
2012-01-17 13:06:40 -0500

    added a few helpful links

--- web/templates/_moreinfo.inc.php
+++ web/templates/_moreinfo.inc.php
@@ -1,5 +1,7 @@
 <h3>More Information</h3>
 <ul>
-	<li> <a href="https://github.com/dmolsen/Detector">Detector on GitHub</a></li>
 	<li> <a href="http://twitter.com/dmolsen/">@dmolsen on Twitter</a></li>
+	<li> <a href="https://github.com/dmolsen/Detector">Detector on GitHub</a></li>
+	<li> <a href="http://www.opendeviceknowledge.com/vision">Open Device Knowledge Collaborative</a></li>
+	<li> <a href="http://www.lukew.com/ff/entry.asp?1392">RESS: Responsive Design + Server Side Components</a></li>
 </ul>
