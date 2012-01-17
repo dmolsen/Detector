4fe51ac6fe91ea2a2167737e9a11fd96cd385a53
Dave Olsen <dmolsen@gmail.com>
2012-01-17 10:34:49 -0500

    added a real archive page

--- web/templates/_archive.inc.php
+++ web/templates/_archive.inc.php
@@ -1,11 +1,12 @@
 <h3>Archive</h3>
 <p>
-	The following user agent profiles are already in the system:
+	The following user agent profiles are already in the system (<a href="archive.php">readable list</a>):
 </p>
 <ul>
 	<?php
 		if ($uaListJSON = @file_get_contents(__DIR__."/../../lib/Detector/user-agents-core/ua.list.json")) {
-			$uaList = json_decode($uaListJSON);
+			$uaList = (array) json_decode($uaListJSON);
+			asort($uaList);
 			foreach ($uaList as $key => $value) {
 				print "<li> <a href=\"/?pid=".$key."\">".trim(substr($value, 0, 28))."...</a></li>";
 			}
