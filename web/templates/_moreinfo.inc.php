3970cca4ae83a9478b489f2eb7da4ca1abadb230
Dave Olsen <dmolsen@gmail.com>
2012-01-18 09:02:47 -0500

    added a link to the article about detector on my blog

--- web/templates/_moreinfo.inc.php
+++ web/templates/_moreinfo.inc.php
@@ -4,4 +4,5 @@
 	<li> <a href="https://github.com/dmolsen/Detector">Detector on GitHub</a></li>
 	<li> <a href="http://www.opendeviceknowledge.com/vision">Open Device Knowledge Collaborative</a></li>
 	<li> <a href="http://www.lukew.com/ff/entry.asp?1392">RESS: Responsive Design + Server Side Components</a></li>
+	<li> <a href="http://www.dmolsen.com/mobile-in-higher-ed/?p=318">Why I Created Detector</a></li>
 </ul>
