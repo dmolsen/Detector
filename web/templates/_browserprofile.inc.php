637595214515aba79becc2e03b58b6076c8629f3
Dave Olsen <dmolsen@gmail.com>
2012-02-07 19:59:47 -0500

    removed the "where we found this profile" to it's own section

--- web/templates/_browserprofile.inc.php
+++ web/templates/_browserprofile.inc.php
@@ -1,19 +1,7 @@
 <h3><?= (Detector::$foundIn == 'archive') ? 'Archived' : 'Your'; ?> Detector Browser Profile</h3>
 <p>
-	The following browser profile was created using the browser-detection half of Detector. It, as well as the the following feature profile, were <strong>
-	<?php
-		if (Detector::$foundIn == "archive") {
-			print " pulled from a profile already in the system that you asked to view. Because it's an archived profile the browser-side tests were not run.";
-		} else if (Detector::$foundIn == 'cookie') {
-			print " created when you first hit this page because Detector didn't recognize your user-agent. You may have experienced a very brief redirect when loading the page initially. The profiles have now been saved for use with other visitors.";
-		} else if (Detector::$foundIn == 'file') {
-			print " created in the past when another user with the same user-agent visited this demo. Detector simply pulled the already existing information for your visit.";
-		} else if (Detector::$foundIn == 'nojs') {
-			print " created when you first hit this page because Detector didn't recognize your user-agent. Because your browser didn't support JavaScript your profiles are very limited. The profiles have now been saved for use with other visitors.";
-		} else {
-			print " pulled from session because you've visited this page before.";
-		}
-	?></strong>
+	The following browser profile was created using <a href="https://github.com/dmolsen/ua-parser-php">ua-parser-php</a>. This information
+	is derived solely from the user agent string for your browser.
 </p>
 <? 
 	if (Detector::$foundIn == 'archive') {
