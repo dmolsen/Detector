8ffe5fea31dce2ea8c6c3543dbc6afd52ba0586b
Dave Olsen <dmolsen@gmail.com>
2012-02-07 19:52:51 -0500

    small tweak to the h3 tag

--- web/templates/_about.inc.php
+++ web/templates/_about.inc.php
@@ -1,4 +1,4 @@
-<h3>About Detector</h3>
+<h3 style="margin-top: -11px;">About Detector</h3>
 <p>
 	<a href="https://github.com/dmolsen/Detector">Detector</a> is a simple, PHP- and JavaScript-based browser- and feature-detection library. Detector gives
 	server-side developers information about what types of devices may be requesting their content as well as the HTML5 &amp; CSS3 features a requesting browser may or may not support. With Detector a developer 
