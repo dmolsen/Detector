3603ed113b4e4151e9f1b3902d633d36c00358c0
Dave Olsen <dmolsen@gmail.com>
2012-02-17 10:22:36 -0500

    added the link to the wiki article on RESS templating

--- web/demo/mustache/index.php
+++ web/demo/mustache/index.php
@@ -11,7 +11,7 @@ $template = file_get_contents("templates/index.mustache");
 $data = array(
 	'title'       => 'Hello, World!',
 	'description' => 'This extremely simple demo is meant to show how Detector and Mustache can be combined to create a Responsive Web Design + Server Side Component (RESS) System.',
-	'link'        => '#',
+	'link'        => 'https://github.com/dmolsen/Detector/wiki/Templating-with-Detector-&-Mustache-Tutorial',
 	'images'      => array(
 		               array('index'=>'1','title'=>'Automobile','alt'=>'auto','img'=>'images/automobile.jpg','img_sml'=>'images/automobile_sml.jpg','src'=>'http://farm4.staticflickr.com/3347/3411775886_fcf0af1a42_z.jpg'),
 		               array('index'=>'2','title'=>'Bus','alt'=>'bus','img'=>'images/bus.jpg','img_sml'=>'images/bus_sml.jpg','src'=>'http://www.flickr.com/photos/d0a98042/3774873571/sizes/z/in/photostream/'),
