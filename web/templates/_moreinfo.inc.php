8ee03760dce8c3dc439487c439027b6f56d4fc21
Dave Olsen <dmolsen@gmail.com>
2012-02-17 13:12:14 -0500

    added a link to the code, demos, and tutorials

--- web/templates/_moreinfo.inc.php
+++ web/templates/_moreinfo.inc.php
@@ -1,9 +1,26 @@
+<h3>Get the Code</h3>
+<ul>
+	<li> <a href="https://github.com/dmolsen/Detector">Detector on GitHub</a></li>
+</ul>
+
+<h3>Demos</h3>
+<ul>
+    <li> <a href="/demo/mustache/">RESS Demo with Mustache</a></li>
+	<li> <a href="/demo/ytembed/">YouTube Embed Example</a></li>
+</ul>
+
+<h3>Tutorials</h3>
+<ul>
+	<li> <a href="https://github.com/dmolsen/Detector/wiki/Adding-&-Using-Detector-With-Your-Application">Adding &amp; Using Detector With Your Application</a></li>
+	<li> <a href="https://github.com/dmolsen/Detector/wiki/Detector-Test-Tutorial">Creating Tests with Detector</a></li>
+	<li> <a href="https://github.com/dmolsen/Detector/wiki/Detector-Family-Tutorial">Creating Browser Groupings with Families</a></li>
+	<li> <a href="https://github.com/dmolsen/Detector/wiki/Templating-with-Detector-&-Mustache-Tutorial">Templates with Detector &amp; Mustache</a></li>
+</ul>
+	
 <h3>More Information</h3>
 <ul>
-	<li> <a href="http://twitter.com/dmolsen/">@dmolsen on Twitter</a></li>
-	<li> <a href="https://github.com/dmolsen/Detector">Detector on GitHub</a></li>
 	<li> <a href="http://www.opendeviceknowledge.com/vision">Open Device Knowledge Collaborative</a></li>
 	<li> <a href="http://www.lukew.com/ff/entry.asp?1392">RESS: Responsive Design + Server Side Components</a></li>
-	<li> <a href="/demo/ytembed/">YouTube Embed Example</a></li>
 	<li> <a href="http://www.dmolsen.com/mobile-in-higher-ed/?p=318">Why I Created Detector</a></li>
+	<li> <a href="http://twitter.com/dmolsen/">@dmolsen on Twitter</a></li>
 </ul>
