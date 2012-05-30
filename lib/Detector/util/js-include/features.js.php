1408820d120914ede239b8e12d8ad12eb7697aa1
Dave Olsen <dmolsen@gmail.com>
2012-05-30 11:59:18 -0400

    turns off the Detector::build() function when making the perrequest call

--- lib/Detector/util/js-include/features.js.php
+++ lib/Detector/util/js-include/features.js.php
@@ -1,5 +1,6 @@
 <?php 
     // to be included in your <head></head> tag to run the per-request tests
+	$p = true; // turn off the build function
 	require("path/to/Detector/Detector.php");
 	header("content-type: application/x-javascript");
 	Detector::perrequest();
